58b0388f835319d61a2e40fc9aa315de42608e24
NatiGK <natnaelgashu2022@gmail.com>
2023-08-22 11:04:01 +0300

    fix issues

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -8,9 +8,14 @@ import { group } from 'console';
 export class ChatService {
   constructor(private prisma:PrismaService){}
   findAll(myId:number,type:"group"|"user") {
+    //if chat is with user
+    //return chats which include my id
     const filter:any = {AND:[{OR:[{user1Id:myId},{user2Id:myId}]}]}
+
+    //else if hat is in group
+    // include the chat where i am a member of
     if(type==="group"){
-      filter.AND[0] = {NOT:{groupId:null}}
+      filter.AND[0] = {AND:[{NOT:{groupId:null}},{group:{members:{some:{userId:myId}}}}]}
     }
     return this.prisma.chat.findMany({
       where:filter,
@@ -26,7 +31,21 @@ export class ChatService {
     })
   }
   async findOne(myId:number,chatId:number){
-    return this.prisma.chat.findUnique({where:{id:chatId,OR:[{user1Id:myId},{user2Id:myId}]},include:{user1:true,user2:true,group:true}})
+    return this.prisma.chat.findUnique({
+      where:{
+        id:chatId,
+        OR:[
+          {user1Id:myId},
+          {user2Id:myId},
+          {group:{members:{some:{userId:myId}}}}
+        ]
+      },
+      include:{
+        user1:true,
+        user2:true,
+        group:true
+      }
+    })
   }
   async getChatWithUser(myId:number,userId:number){
     const chat = await this.prisma.chat.findFirst({where:{OR:[{user1Id:myId,user2Id:userId},{user2Id:myId,user1Id:userId}]}})
